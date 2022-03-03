f6680548f5fd27357e462580d112799c572ace02
karthikbenki <karthikmc007@gmail.com>
2022-03-03 23:18:35 +0530

    Added: RegexValidation Logic

---
+++ src/RegexValidation.java
@@ -0,0 +1,13 @@
+import java.util.regex.Pattern;
+
+public class RegexValidation {
+    static public boolean isValid(String regex,String input) {
+        Pattern pattern= Pattern.compile(regex);
+        return pattern.matches(regex,input);
+
+    }
+    public static void main(String[] args) {
+        boolean regexCheck = isValid("^[1-9]{1}[\\d]{5}$","400098");
+        System.out.println(regexCheck);
+    }
+}
